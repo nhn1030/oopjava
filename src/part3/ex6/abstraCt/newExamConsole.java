fd03d02acd80d2617b33d689e0aefa4a731c397b
nhn1030 <shgusska12@gmail.com>
2023-08-10 11:47:01 +0900

    이벤트 메서드

--- src/part3/ex6/abstraCt/newExamConsole.java
+++ src/part3/ex6/abstraCt/newExamConsole.java
@@ -1,9 +1,38 @@
 package src.part3.ex6.abstraCt;
 
+import java.util.Scanner;
+
 public class newExamConsole extends ExamConsole{
 
     @Override
     protected Exam makeExam() {
         return new newExam();
+    }
+
+    @Override
+    protected void onPrint(Exam exam) {
+        newExam newexam = (newExam) exam;
+        int com = newexam.getCom();
+        System.out.printf("컴퓨터 : %d\n", com);
+    }
+
+    @Override
+    protected void onInput(Exam exam) {
+
+        newExam newexam = (newExam) exam;
+        Scanner scan = new Scanner(System.in);
+
+        int com;
+
+        do {
+            System.out.print("컴퓨터 점수 입력 : ");
+            com = scan.nextInt();
+
+            if (com < 0 || 100 < com) {
+                System.out.println("성적 범위를 벗어났습니다. 다시 입력하세요.");
+            }
+        } while (com < 0 || 100 < com);
+
+        newexam.setCom(com);
     }
 }
